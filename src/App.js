f06eb969d5f8a828adfa5a3a4de7fdbee057bfc9
mrashishraj <mr.ashishrajyadav@gmail.com>
2022-01-09 07:58:07 +0530

    material component added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
 import './App.css';
+import React, { useState } from "react"
+import {Input,Box, Grid,TextField,Button, Typography} from '@mui/material';
+import UnstyledButtonCustom from "./Component/button"
+import { color, grid } from '@mui/system';
 
-function App() {
+const ariaLabel = { 'aria-label': 'description' };
+
+
+export default function App() {
+  const [inputList, setInputList] = useState()
   return (
-    <div className="main-container">
-      <div className="main-div">
-          <br/>
-          <h1>ToDo List</h1>
-          <br/>
-          <input type="text" placeholder="Add a List" />
-          <button>+</button>
-      </div>
-    </div>
+    <Grid container justify="center" alignItems="center">
+      <Grid xs={4} alignItems="center" style={{textAlign:"center" ,border:"1px solid black", padding:"20px",borderRadius:"16px",marginTop:"20%", marginLeft:"20%"}} >
+      <Grid item>
+        <Typography variant='h5' color="primary">
+          ToDo List App
+        </Typography>
+      </Grid>
+      <Grid item >
+      <TodoContainer/>
+      </Grid>
+      </Grid>
+    </Grid>
   );
 }
 
-export default App;
+
+const TodoContainer = () =>{
+  return(
+    <Grid container  alignItems="center" justify="center">
+      <Grid item xs={10}><TextField 
+      id="standard-basic" 
+      label="Add ToDo List" 
+      variant="standard" 
+      fullWidth 
+      sx={{margin:"10px"}}>
+      </TextField></Grid>
+      <Grid item xs={2}><Button variant="text">
+        <i style={{fontSize:"25px"}} class="fas fa-plus-square"></i>
+      </Button></Grid>
+      
+      
+
+    </Grid>
+  )
+} 
+
